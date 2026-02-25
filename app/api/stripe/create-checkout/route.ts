733eb4187ad2671466c548e9ab180c5236f976ee
76248usa <elsabecrous@Elsabes-MacBook-Pro.local>
2026-02-25 09:45:21 -0600

    deleted api version

--- app/api/stripe/create-checkout/route.ts
+++ app/api/stripe/create-checkout/route.ts
@@ -7,7 +7,7 @@ export const runtime = "nodejs";
 //const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  //apiVersion: "2024-06-20",
 });
 
 export async function GET() {
