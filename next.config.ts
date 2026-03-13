68a455814d3f4930ac577368a4e0265fa65a97db
76248usa <elsabecrous@Elsabes-MacBook-Pro.local>
2026-03-13 14:48:55 -0500

    changed redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source:
+          "/eye-drop-days-supply-guides/brimonidine-tartrate-timolol-maleate",
+        destination: "/how-to-calculate-eye-drop-days-supply",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
